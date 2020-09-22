d122aa5843e4a89a8544a1fefbafe3ae05b31cf3
knigawkl <lukasz.knigawka@outlook.com>
2020-09-22 13:18:06 +0200

    read params with getopt

--- generate_input.cpp
+++ generate_input.cpp
@@ -1,8 +1,13 @@
 #include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <string>
 #include <string_view>
 #include <iostream>
 #include <sstream>
+#include <cstdlib>
+
+#include "generate_input.hpp"
 
 #include <opencv2/opencv.hpp>
 
@@ -28,14 +33,57 @@ void save_input() {
 
 }
 
-int main() {
-    // todo: move configuration to separate file
-    constexpr uint8_t const segment_size = 50;
-    constexpr uint8_t const segment_cnt = 18;
-    std::string const input_video = "/home/lk/Desktop/praca-inzynierska/gmcp-tracker-cpp-python/fixtures/input/pets3s.mp4";
-    std::string const output_video = "/home/lk/Desktop/praca-inzynierska/gmcp-tracker-cpp-python/fixtures/output/output.mp4";
+int main(int argc, char **argv) {
+    uint8_t segment_size;
+    uint8_t segment_cnt;
+    std::string input_video;
+    std::string output_video;
 
-    // this configuration may be local
+    const char* usage_info =
+    "    -s, --segment_size   frames in a segment\n"
+    "    -c, --segment_cnt    number of segments\n"
+    "    -i, --input_video    frames in a segment\n"
+    "    -o, --output_video   frames in a segment\n"
+    "    -h, --help           show this help msg";
+
+    int opt;
+    while((opt = getopt(argc, argv, "s:c:i:o:h")) != -1)
+    {
+        switch (opt)
+        {
+            case 's':
+                segment_size = std::stoi(optarg);
+                break;
+            case 'c':
+                segment_cnt = std::stoi(optarg);
+                break;
+            case 'i':
+                input_video = optarg;
+                break;
+            case 'o':
+                output_video = optarg;
+                break;
+            case 'h':
+                std::cout << "Please provide the following arguments:\n" << usage_info;
+                exit(0);
+            default:
+                std::cout << "Unsupported parameter passed to the script. Aborting.";
+                std::cout << usage_info;
+                abort();
+        }
+    }
+
+    if (segment_cnt == 0)
+        segment_cnt = 18;
+    if (segment_size == 0)
+        segment_size = 10;
+    if (input_video == "")
+        input_video = "/home/lk/Desktop/praca-inzynierska/gmcp-tracker-cpp-python/fixtures/input/pets3s.mp4";
+    if (output_video == "")
+        output_video = "/home/lk/Desktop/praca-inzynierska/gmcp-tracker-cpp-python/fixtures/output/output.mp4";
+
+    printf("Running with configuration:\nsegment_size = %d\nsegment_cnt = %d\ninput_video = %s\noutput_video = %s\n", segment_size, segment_cnt, input_video.c_str(), output_video.c_str());
+
     std::string const trimmed_video = "/home/lk/Desktop/praca-inzynierska/gmcp-tracker-cpp-python/fixtures/tmp/tmp.mp4";
 
     cv::VideoCapture in_cap(input_video);
@@ -45,4 +93,3 @@ int main() {
 
     return 0;
 }
-
---
+++ generate_input.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+struct Location
+{
+    int x, y;
+};
