f0bcd18869ed175434cdaab662769a43cfa996ad
knigawkl <lukasz.knigawka@outlook.com>
2020-09-22 11:15:50 +0200

    cmake build

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gmcp-tracker-cpp-python
 A custom C++/Python GMCP tracking implementation
+
+Build with CMake.
+> mkdir build
+> cd build
+> cmake ../
+> make -j8 (substitute 8 with numprocs)
--- generate_input.cpp
+++ generate_input.cpp
@@ -1,13 +1,21 @@
 #include <stdint.h>
 #include <string>
 #include <string_view>
+#include <iostream>
 #include <sstream>
 
+#include <opencv2/opencv.hpp>
 
-void trim_video(std::string video_in, std::string video_out) {
-    std::string frames_cnt = "10";	
+
+int get_trimmed_frames_cnt(const cv::VideoCapture& cap, uint8_t frames_in_segment) {
+    int video_in_frame_cnt = cap.get(cv::CAP_PROP_FRAME_COUNT);
+    std::cout << "Input video frames count: " << video_in_frame_cnt << std::endl;
+    return video_in_frame_cnt / frames_in_segment * frames_in_segment;
+}
+
+void trim_video(std::string video_in, std::string video_out, int frames_cnt) {	
     std::stringstream ss;
-    ss << "ffmpeg -i " << video_in << " -vframes " << frames_cnt << " -acodec copy -vcodec copy " << video_out << " -y";
+    ss << "ffmpeg -i " << video_in << " -vframes " << std::to_string(frames_cnt) << " -acodec copy -vcodec copy " << video_out << " -y";
     std::string trim_command = ss.str();
     system(trim_command.c_str());
 }
@@ -30,7 +38,11 @@ int main() {
     // this configuration may be local
     std::string const trimmed_video = "/home/lk/Desktop/praca-inzynierska/gmcp-tracker-cpp-python/fixtures/tmp/tmp.mp4";
 
-    trim_video(input_video, trimmed_video);
+    cv::VideoCapture in_cap(input_video);
+    auto trimmed_video_frames_cnt = get_trimmed_frames_cnt(in_cap, segment_size);
+    trim_video(input_video, trimmed_video, trimmed_video_frames_cnt);
+    std::cout << "Input video frames count cut to: " << trimmed_video_frames_cnt << std::endl;
 
     return 0;
 }
+
