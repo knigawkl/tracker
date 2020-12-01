57d2173d2e2cc5cdf05e3451c3c58130ac8972da
knigawkl <lukasz.knigawka@outlook.com>
2020-12-01 22:49:29 +0100

    add test images

---
+++ detectors/compare.py
@@ -0,0 +1,5 @@
+from run_fchd import FCHDDetector
+from run_yolo import YOLODetector
+from run_ssd import SSDDetector
+
+IMAGE_PATH = 
--- detectors/fchd/head_detection_demo.py
+++ detectors/fchd/head_detection_demo.py
@@ -34,25 +34,7 @@ def read_img(path):
     img = preprocess(img)
     _, o_H, o_W = img.shape
     scale = o_H / H
-    return img, img_raw_final, scale
-
-def detect(img_path, model_path):
-    file_id = utils.get_file_id(img_path)
-    img, img_raw, scale = read_img(img_path)
-    head_detector = Head_Detector_VGG16(ratios=[1], anchor_scales=[2,4])
-    trainer = Head_Detector_Trainer(head_detector).cuda()
-    trainer.load(model_path)
-    img = at.totensor(img)
-    img = img[None, : ,: ,:]
-    img = img.cuda().float()
-    st = time.time()
-    pred_bboxes_, _ = head_detector.predict(img, scale, mode='evaluate', thresh=THRESH)
-    et = time.time()
-    tt = et - st
-    print ("[INFO] Head detection over. Time taken: {:.4f} s".format(tt))
-    for i in range(pred_bboxes_.shape[0]):
-        ymin, xmin, ymax, xmax = pred_bboxes_[i,:]
-        utils.draw_bounding_box_on_image_array(img_raw,ymin, xmin, ymax, xmax)  
+    return img, img_raw_final, scale 
 
 
 if __name__ == "__main__":
@@ -61,22 +43,7 @@ if __name__ == "__main__":
     parser.add_argument("--model_path", type=str, default='./checkpoints/sess:2/head_detector08120858_0.682282441835')
     args = parser.parse_args()
     detect(args.img_path, args.model_path)
-    # model_path = './checkpoints/sess:2/head_detector08120858_0.682282441835'
 
-    # test_data_list_path = os.path.join(opt.data_root_path, 'brainwash_test.idl')
-    # test_data_list = utils.get_phase_data_list(test_data_list_path)
-    # data_list = []
-    # save_idx = 0
-    # with open(test_data_list_path, 'rb') as fp:
-    #     for line in fp.readlines():
-    #         if ":" not in line:
-    #             img_path, _ = line.split(";")
-    #         else:
-    #             img_path, _ = line.split(":")
-
-    #         src_path = os.path.join(opt.data_root_path, img_path.replace('"',''))
-    #         detect(src_path, model_path, save_idx)
-    #         save_idx += 1
 
 
 
